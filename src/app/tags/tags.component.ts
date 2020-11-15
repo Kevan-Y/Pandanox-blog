63ae8436ba541505774aeb9c84f3dac09fa6ddff
Pandanox <58233223+Pandanoxes@users.noreply.github.com>
2020-11-15 13:28:35 -0500

    Updating Tag component with live blog data

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-tags',
@@ -6,18 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tags.component.css'],
 })
 export class TagsComponent implements OnInit {
-  constructor() {}
-  tags: Array<string> = [
-    '#funny',
-    '#dramatic',
-    '#rental',
-    '#seeagain',
-    '#spooky',
-    '#worththecost',
-    '#lovedIt',
-    '#scary',
-    '#silly',
-    '#good4kidz',
-  ];
-  ngOnInit(): void {}
+  constructor(private _postService: PostService) {}
+  tags: Array<string>;
+  ngOnInit(): void {
+    this._postService.getTags().subscribe((data) => (this.tags = data));
+  }
 }
